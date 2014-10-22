39dd8853d94ee73cd4e0f6b93d1b94e501f8b10f
alexfakhri <alexfakhri@me.com>
2014-10-22 20:10:04 +0100

    Added on and off switch for power saver mode

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -3,10 +3,14 @@
 describe('Thermostat', function() {
 	var thermostat;
 
+		beforeEach(function(){
+			thermostat = new Thermostat();
+		});
+
 	describe('by default', function() {
 
+
 		it('is set to 20 degrees', function() {
-			thermostat = new Thermostat();
 			expect(thermostat.temperature).toEqual(20);
 		});
 
@@ -20,7 +24,40 @@ describe('Thermostat', function() {
 	});
 
 
+	describe('Custom option', function() {
 
+		it('can increase the temperature by on degree', function() {
+			thermostat.increaseTemperature();
+			expect(thermostat.temperature).toEqual(21)
+		});
+
+		it('can decrease the temperature by on degree', function() {
+			thermostat.decreaseTemperature();
+			expect(thermostat.temperature).toEqual(19)
+		});
+
+		it('can increase the temperature by 10 degrees', function(){
+			thermostat.increaseTemperatureBy(10);
+			expect(thermostat.temperature).toEqual(30);
+		});
+
+		it('can decrease the temperature by 10 degrees', function(){
+			thermostat.decreaseTemperatureBy(10);
+			expect(thermostat.temperature).toEqual(10);
+		});
+
+		it('power saving mode can be turned off', function(){
+			thermostat.powerSaverButton("off");
+			expect(thermostat.isPowerSaverOn).toBe(false);
+		});
+
+		it('power saver mode can be switched back on', function() {
+			thermostat.powerSaverButton("on");
+			expect(thermostat.isPowerSaverOn).toBe(true);
+		});
+
+
+	});
 
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -3,3 +3,26 @@ function Thermostat() {
 	this.minimumTemperature = 10;
 	this.isPowerSaverOn = true;
 };
+
+Thermostat.prototype.increaseTemperature = function() {
+	this.increaseTemperatureBy(1);
+};
+
+Thermostat.prototype.decreaseTemperature = function() {
+	this.decreaseTemperatureBy(1);
+};
+
+Thermostat.prototype.increaseTemperatureBy = function(degrees) {
+	this.temperature += degrees
+	
+};
+Thermostat.prototype.decreaseTemperatureBy = function(degrees) {
+	this.temperature -= degrees;
+	
+};
+
+
+Thermostat.prototype.powerSaverButton = function(onOff) {
+	if (onOff === "off") return this.isPowerSaverOn = false;
+	if (onOff === "on") return this.isPowerSaverOn;
+};
