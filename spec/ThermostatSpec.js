eab0e1e0cf58d52243165461143506ae23feb0ad
alexfakhri <alexfakhri@me.com>
2014-10-22 19:24:17 +0100

    Created thermostat using jasmine testing and initialized it with default thing

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,18 @@
+"use strict";
+
+describe('Thermostat', function() {
+	var thermostat;
+
+	describe('by default', function() {
+
+		it('is set to 20 degrees', function() {
+			thermostat = new Thermostat();
+			expect(thermostat.temperature).toEqual(20);
+		});
+
+
+
+	});
+
+
+});
